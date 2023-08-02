b5e249ddee9f9084518b0cd896bb8c4c996c7196
Saurabh <srb.deshpande@gmail.com>
2023-08-02 14:15:03 +0100

    multithreading implemented for bikes, categories, refrences

--- api_functions.py
+++ api_functions.py
@@ -40,16 +40,69 @@ def connect_to_api():
 
 
 def get_bikes():
-    # API request to retreive list of bikes
-    response_bikes = requests.get(
-        'https://api.puig.tv/en/bikes', headers=header)
-    # Convert list of bikes into dataframe
-    bikes_df = pd.DataFrame(response_bikes.json()['data'])
-    bikes_df['puig_final_name'] = bikes_df['brand'].astype(
-        str)+" "+bikes_df['model'].astype(str)+" "+bikes_df['year'].astype(str)
+    try:
+        # API request to retreive list of bikes
+        response = requests.get(
+            'https://api.puig.tv/en/bikes', headers=header)
+        if response.status_code == 200:
+            # Convert list of bikes into dataframe
+            bikes_df = pd.DataFrame(response.json()['data'])
+            bikes_df['puig_final_name'] = bikes_df['brand'].astype(
+                str)+" "+bikes_df['model'].astype(str)+" "+bikes_df['year'].astype(str)
 
-    bikes_df['puig_final_name'] = bikes_df['puig_final_name'].str.replace(
-        '  ', ' ')
-    # db_display(bikes_df, "bikes")
-    sh_write(bikes_df, "PUIG", "bikes")
-    print(bikes_df)
+            bikes_df['puig_final_name'] = bikes_df['puig_final_name'].str.replace(
+                '  ', ' ')
+            db_write(bikes_df, "bikes")
+            sh_write(bikes_df, "PUIG", "bikes")
+            print("bikes")
+    except Exception as e:
+        send_email("PUIG API script failed.",
+                   f"Function get_bikes() failed.\nNo immediate action necessary.\nThe script will auto-retry after a delay.\nDo ensure the script has executed successfully after a while.\n{e}")
+        raise e
+# -------------------------------------------------------------------------------------------------------------------------------
+
+# API REQUESTS TO GET CATEGORIES
+# -------------------------------------------------------------------------------------------------------------------------------
+
+
+def get_categories():
+    try:
+        response = requests.get(
+            'https://api.puig.tv/en/categories', headers=header)
+        if response.status_code == 200:
+            # Convert list of bikes into dataframe
+            categories_df = pd.DataFrame(response.json()['data'])
+            db_write(categories_df, "categories")
+            sh_write(categories_df, "PUIG", "categories")
+            print("categories")
+    except Exception as e:
+        send_email("PUIG API script failed.",
+                   f"Function get_categories() failed.\nNo immediate action necessary.\nThe script will auto-retry after a delay.\nDo ensure the script has executed successfully after a while.\n{e}")
+        raise e
+# -------------------------------------------------------------------------------------------------------------------------------
+
+# API REQUESTS TO GET REFERENCES(all REFERENCE sku's)
+# -------------------------------------------------------------------------------------------------------------------------------
+
+
+def get_references():
+    try:
+        # API request to retreive list of references
+        response = requests.get(
+            'https://api.puig.tv/en/references', headers=header)
+        if response.status_code == 200:
+            # Convert list of references into dataframe
+            references_df = pd.DataFrame(response.json()['data'])
+            references_df.rename(columns={0: 'ref_sku'}, inplace=True)
+            # Droping known sku with error
+            references_df.drop(
+                references_df[references_df.ref_sku == "5020N/G"].index, inplace=True)
+            references_df.drop_duplicates(
+                subset=['ref_sku'], keep="first", inplace=True)
+            db_write(references_df, "references")
+            sh_write(references_df, "PUIG", "references")
+            print("references")
+    except Exception as e:
+        send_email("PUIG API script failed.",
+                   f"Function get_references() failed.\nNo immediate action necessary.\nThe script will auto-retry after a delay.\nDo ensure the script has executed successfully after a while.\n{e}")
+        raise e
--- api_main.py
+++ api_main.py
@@ -1,12 +1,27 @@
 from api_functions import *
 from api_data_read_write import *
 import time
+import asyncio
+import threading
+
 
 if __name__ == '__main__':
     t0 = time.time()
-    # connect_to_db()
+    connect_to_db()
     connect_to_serv_acc()
     connect_to_api()
-    get_bikes()
+
+    all_threads = []
+    # Create a thread for each function
+    for func in [get_bikes, get_categories, get_references]:
+        t = threading.Thread(target=func)
+        all_threads.append(t)
+    # Start all the threads
+    for t in all_threads:
+        t.start()
+    # Wait for all threads to finish
+    for t in all_threads:
+        t.join()
+
     t1 = time.time()
     print(f"Execution Time: {(t1-t0)/60} minutes")
